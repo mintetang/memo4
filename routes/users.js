0c0f8cc7954b946cf0aa71eb64bca02e265fee6e
mintetang <mintetang@gmail.com>
2025-11-13 11:38:45 +0800

    test routes

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('will do it later');
+});
+router.get('/cool/', function(req, res, next) {
+  res.send('I am cool!');
 });
 
 module.exports = router;
